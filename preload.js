608c7d6152eaf8e54c30f0a55f0e303597e32ae2
himanggurbani <himanggurbani.123098@gmail.com>
2026-05-21 12:53:40 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  setSystemVolume: (vol) => ipcRenderer.send('control-system-volume', vol),
-  switchWindow: () => ipcRenderer.send('switch-active-window')
-});
